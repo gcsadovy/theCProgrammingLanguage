542155afcb4e3845b0db63c0c47f02fa70840a39
Garik <sadovyg@gmail.com>
2017-09-12 03:17:36 -0400

    Add files via upload

---
+++ findLengthAllVariableTypes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+/* determine the ranges of char, short, int, and long variables, both signed and unsigned, by printing appropriate values from standard headers and by direct computation */
+
+/* use while loops to add characters 1 by 1 to each type of variable until no more can be added and then spit out i for each character type */
+/* each while loop can be a different function */
+
+main() {
+    
+}
+
+
+
+
