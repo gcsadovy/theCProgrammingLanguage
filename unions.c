11729f473817c950fd72404e2bd84d1ecdcb83fe
Garik <sadovyg@gmail.com>
2017-09-26 16:48:18 -0400

    Add files via upload

--- unions.c
+++ unions.c
@@ -24,4 +24,55 @@ union u_tag {
 //any of these types may be assigned to u and then used in expressions, so long as the usage is consistent; the type retrived must be the type most recently stored
 
 //it is the programmers respondibility to kee[ track of which type is currently stored in a union; the results are implementaion-deoendent if something is stored as one type and extracted as another
+//syntactaclly members of a union are accessed as
+
+//union-name.member
+
+//or
+
+//union-pointer->member
+
+//just as for structures; if the variable utype is used to kee- track of the current type in stored in u, then on might see code such as
+
+if (utype == INT)
+  printf("%d\n", u.ival);
+if (utype == FLOAT)
+  printf("%f\n", u.fval);
+if (utype == STRING)
+  printf("%s\n", u.sval);
+ else
+   printf("bad type %d in utype\n", utype);
+
+//unions may occur within structures and arrays, and vice versa; the notation for accessing a member of a union in a structure (or vice versa) is identical to that for nested structures
+
+//in the structure array defined by:
+
+struct {
+  char *name;
+  int flags;
+  int utype;
+  union {
+    int ival;
+    float fval;
+    char *sval;
+  } u;
+} symtab[NSYM];
+
+//the member ival is referred to as
+
+symtab[i].u.ival
+
+//and thje first character opf thje string sval by either of
+
+*symtab[i].u.sval
+
+symtab[i].u.sval[0]
+
+//in effect a union is a structure in which all members have offset zero from the base, the structure is big enough to hold the widest member, and the alignment is appropriate for all of the types in the union
+
+//functions are the same as with a structure: assignment to or copying as a unit, taking the address, and accessing a member
+
+//union may only be initialized with a value of the ty[e of its first member; thus union u described above can only be initialized with an integer value
+
+//the storage allocator in chapter 8 shows how a union can be used to force a variable to be a;igned on a particular kind of storage boundary
 
